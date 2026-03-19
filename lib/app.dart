9ca183cb62eea2ddd50425026102188b7cb85eaa
Francis6h057 <ngopsyjr2@gmail.com>
2026-03-19 15:05:44 +0200

    Setup app entry point and base structure

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Routine App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Routine App")),
+        body: const Center(child: Text("App Initialized")),
+      ),
+    );
+  }
+}
